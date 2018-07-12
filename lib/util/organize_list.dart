3ed12d9d62dc61635d65cef50f52d1ba8dccf3f3
SamarthDesai01 <37949934+SamarthDesai01@users.noreply.github.com>
2018-07-12 14:28:16 -0500

    Fix color sort and implement alphabetical sort

--- lib/util/organize_list.dart
+++ lib/util/organize_list.dart
@@ -43,18 +43,42 @@ List<Reminder> organizeByColor(List<Reminder> currentList){
     }
     //Loop through our sorted array looking for color matches, 
     //if one is found insert the current reminder right after the matched element
-    while(currentIndex < currentList.length && !addedReminder){ 
+    while(currentIndex < sortedReminders.length && !addedReminder){ 
       Reminder reminderInSorted = sortedReminders[currentIndex];
       if(reminderInSorted.cardColor == currentReminder.cardColor){
         sortedReminders.insert(currentIndex+1, currentReminder);
         addedReminder = true;
-      }else if(currentIndex == currentList.length - 1){ //Looped through all the elements but still wasn't able to find a match. Just add to the end of the array as a result
+      }else if(currentIndex == sortedReminders.length - 1){ //Looped through all the elements but still wasn't able to find a match. Just add to the end of the array as a result
         sortedReminders.add(currentReminder);
+        addedReminder = true;
       }
       currentIndex++; //Increment our index in sortedReminders 
     }
     currentIndex = 0; //Reset while conditions for next for loop iteration
     addedReminder = false; 
+  }
+  return sortedReminders;
+}
+
+List<Reminder> organizeByAlphabet(List<Reminder> currentList){
+    List<Reminder> sortedReminders = [];
+    List<List<dynamic>> sortReminders = [];
+  if(currentList.length == 0){
+    return sortedReminders = [];
+  }
+  //Group together all the current notifications by title and their position in the list
+  for(int i = 0; i < currentList.length; i++){
+    Reminder currentReminder = currentList[i];
+    sortReminders.add([currentReminder.cardTitle, i]);
+  }
+
+  //Sort the list by ascending alphabetic order
+  sortReminders.sort((a,b) => a[0].compareTo(b[0])); 
+  
+  for(int i = 0; i < sortReminders.length; i++){
+    //Recreate the list using the index stored in the sorted array of arrays to pull from 
+    //the original list and add them as an ordered list of notifications
+    sortedReminders.add(currentList[sortReminders[i][1]]); 
   }
   return sortedReminders;
 }
